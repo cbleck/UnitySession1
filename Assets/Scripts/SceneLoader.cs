f4b7ab80f6e273ee95d20f7b5edfa16bf986379d
cbleck <carlos@capptu.com>
2016-06-17 19:39:39 -0500

    1st commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    void Awake() {
+        Debug.Log("Awake message");
+    }
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+	}
+}
---
+++ Assets/Scripts/Transformer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transformer : MonoBehaviour {
+    
+    public float rotationSpeed;
+	// Use this for initialization
+	void Start () {
+        transform.localScale = new Vector3(3f, 3f, 3f);
+        GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Rotamos el objeto
+        transform.Rotate(
+            0f, 
+            rotationSpeed*Time.deltaTime, 
+            0f, 
+            Space.Self);
+	}
+}
